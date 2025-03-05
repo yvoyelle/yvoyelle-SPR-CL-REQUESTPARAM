f2c67dc78677805e7aaaae33906e404ed0301209
yvon voyelle <Voyelleyvon1@gmail.com>
2025-03-05 01:28:18 +0000

    Completed Lab RequestParam

--- src/main/java/Application/Controller/SearchController.java
+++ src/main/java/Application/Controller/SearchController.java
@@ -38,15 +38,15 @@ public class SearchController {
      * returning 50.
      */
     @GetMapping(value = "cats", params = {"amount"})
-    public int getSearchFormat(){
-        return 0;
+    public int getSearchFormat(@RequestParam int amount){
+        return amount;
     }
     /**
      * TODO: extract the String 'format' and 'orderBy' query parameters from a request, such as
      * GET localhost:9000/cats?format=gif&orderby=new, returning a String array such as {"gif", "new"}
      */
     @GetMapping(value = "cats", params = {"format", "orderBy"})
-    public String[] getSearchFormatAndAmount(){
-        return null;
+    public String[] getSearchFormatAndAmount( @RequestParam String format,  @RequestParam String orderBy){
+        return new String []{format,orderBy};
     }
 }
